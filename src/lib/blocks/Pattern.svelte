0c93267d656384ac22820df609ec5da36ff56e35
Avery Lim <vivianaverylim@gmail.com>
2024-09-12 14:43:16 +0800

    added more block contents

--- src/lib/blocks/Pattern.svelte
+++ src/lib/blocks/Pattern.svelte
@@ -1,1 +1,20 @@
-pattern
+<div />
+
+<style>
+	div {
+		background: repeating-linear-gradient(
+			40deg,
+			rgb(255, 255, 255),
+			rgb(255, 255, 255) 12px,
+			var(--color-accent) 13px,
+			var(--color-accent) 26px
+		);
+		display: block;
+		height: 100%;
+		left: 0;
+		opacity: 0.5;
+		position: absolute;
+		top: 0;
+		width: 100%;
+	}
+</style>
--- src/lib/blocks/Smiley.svelte
+++ src/lib/blocks/Smiley.svelte
@@ -1,1 +1,57 @@
-smiley
+<div class="smiley">
+	<div class="head" />
+</div>
+
+<style>
+	.smiley {
+		display: block;
+		height: 100%;
+		overflow: visible;
+		position: relative;
+		width: 100%;
+	}
+
+	div.head {
+		animation-duration: 5s;
+		animation-iteration-count: infinite;
+		animation-name: rotate;
+		animation-timing-function: linear;
+		border-radius: 50%;
+		clip-path: circle(50% at 50% 50%);
+		cursor: crosshair;
+		position: relative;
+		bottom: 0;
+		right: 0;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+	}
+
+	div.head::before {
+		content: '';
+		background-image: url('/images/smiley.png');
+		background-position: center;
+		background-size: contain;
+		bottom: 0;
+		height: 100%;
+		left: 0;
+		position: absolute;
+		right: 0;
+		top: 0;
+		width: 100%;
+	}
+
+	div.head::before:hover {
+		animation-play-state: paused;
+	}
+
+	@keyframes rotate {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(-359deg);
+		}
+	}
+</style>
