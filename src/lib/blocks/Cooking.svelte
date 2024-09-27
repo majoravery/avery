4552f7efd4a9e3290aa889eda4cb2d8661d4f064
Avery Lim <vivianaverylim@gmail.com>
2024-09-27 11:16:23 +0900

    added cooking image

--- src/lib/blocks/Cooking.svelte
+++ src/lib/blocks/Cooking.svelte
@@ -1,14 +1,27 @@
 <script lang="ts">
 	import { t } from '$lib/stores/locale';
 	import Eyebrow from '$lib/components/Eyebrow.svelte';
+	import cooking from '$lib/images/cooking.png';
 </script>
 
 <article>
 	<Eyebrow>{$t('cooking.title')}</Eyebrow>
+	<div class="dish">
+		<img src={cooking} alt={$t('cooking.title')} />
+	</div>
 </article>
 
 <style>
 	article {
 		display: flex;
+		flex-direction: column;
+	}
+
+	div.dish {
+		position: relative;
+	}
+
+	div.dish img {
+		width: 150%;
 	}
 </style>
