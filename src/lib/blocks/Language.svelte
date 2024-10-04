869806db66066669b1ae5d4f9bd017ce1850392c
Avery Lim <vivianaverylim@gmail.com>
2024-10-04 09:27:29 +0900

    make sure recently photo doesnt stretch

--- src/lib/blocks/Language.svelte
+++ src/lib/blocks/Language.svelte
@@ -73,10 +73,12 @@
 			onDrag: function () {
 				displayTutorial.set(false);
 				interacted.set(true);
-				gsap.set('.language-selector', { rotationY: this.x });
+				gsap.set('.language-selector', { rotationY: this.x + step * activeLanguageIndex });
+				console.log(this.x, step * activeLanguageIndex, this.x + step * activeLanguageIndex);
 			},
 			onDragEnd: function () {
-				const rotationY = gsap.utils.snap(step, this.x);
+				const rotationY = gsap.utils.snap(step, this.x + step * activeLanguageIndex);
+				console.log(this.x, step * activeLanguageIndex, this.x + step * activeLanguageIndex);
 				gsap.to('.language-selector', {
 					rotationY
 				});
