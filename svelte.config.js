dca2b36b329130d11f084e4715d510795bac5fdd
Avery Lim <vivianaverylim@gmail.com>
2024-10-10 09:51:30 +0900

    adapter-static -> adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,17 +8,22 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		// adapter-node config
 		adapter: adapter({
-			// To deploy to Render
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
+			out: 'build',
+			precompress: true,
+			envPrefix: ''
 		}),
+
+		// adapter-static config
+		// adapter: adapter({
+		// 	// To deploy to Render
+		// 	pages: 'build',
+		// 	assets: 'build',
+		// 	fallback: undefined,
+		// 	precompress: false,
+		// 	strict: true
+		// }),
 		prerender: {
 			handleHttpError: 'warn'
 		}
